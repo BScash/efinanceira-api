58027e6ff4e84d0f4cdb7cd3ace25d6ddf6d686e
NickolasMadeiroBscash <nickolas.madeiro@bscash.com.br>
2026-02-05 14:30:20 -0300

    Adicionado: exclude = {UserDetailsServiceAutoConfiguration.class} na anotação @SpringBootApplication Ao reiniciar a aplicação, os warnings não devem mais aparecer.

--- src/main/java/br/com/bscash/efinanceira/EfinanceiraApiApplication.java
+++ src/main/java/br/com/bscash/efinanceira/EfinanceiraApiApplication.java
@@ -2,9 +2,10 @@ package br.com.bscash.efinanceira;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 @EnableJpaRepositories
 public class EfinanceiraApiApplication {
 
